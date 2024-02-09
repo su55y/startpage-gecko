191822031215059001a00c94f0f02961ff587ca1
su55y <cesarmilano88@yahoo.com>
2024-02-10 00:13:42 +0200

    mv

--- js/bookmarks.js
+++ js/bookmarks.js
@@ -1,3 +1,8 @@
+/* exported
+    validateBookmarks
+    filterFolders
+*/
+
 function validateBookmarks(bookmarks) {
   const fail = () => {
     console.warn('Unexpected bookmarks structure')
@@ -50,40 +55,3 @@ function filterFolders(bookmarksArray) {
 
   return folders
 }
-
-function renderBookmarks(folders) {
-  for (const [id, folder] of Object.entries(folders)) {
-    const folderDiv = document.createElement('div')
-    folderDiv.id = id
-    folderDiv.className = 'folder'
-
-    const folderTitle = document.createElement('h3')
-    folderTitle.innerText = folder.title
-    folderDiv.prepend(folderTitle)
-
-    for (const bookmark of folder.bookmarks) {
-      const bookmarkElm = document.createElement('a')
-      bookmarkElm.id = bookmark.id
-      bookmarkElm.href = bookmark.url
-      bookmarkElm.innerText = bookmark.title
-      bookmarkElm.className = 'bookmark'
-      folderDiv.appendChild(bookmarkElm)
-    }
-
-    document.getElementById('root').appendChild(folderDiv)
-  }
-}
-
-function main() {
-  browser.bookmarks.getTree((bookmarksRoot) => {
-    if (!validateBookmarks(bookmarksRoot)) return
-
-    const folders = filterFolders(bookmarksRoot[0].children)
-    for (const [id, folder] of Object.entries(folders))
-      if (!folder.bookmarks.length) delete folders[id]
-
-    renderBookmarks(folders)
-  })
-}
-
-main()
