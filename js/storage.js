3ca4c147a3447301fc398e87ad4cb07c8d570428
su55y <cesarmilano88@yahoo.com>
2024-02-18 13:02:50 +0200

    default theme nord

--- js/storage.js
+++ js/storage.js
@@ -2,17 +2,21 @@
   storage
 */
 
+const nord = {
+  bg: '#2e3440', // nord0
+  bg1: '#3b4252', // nord1
+  bg2: '#434c5e', // nord2
+
+  fg: '#e5e9f0', // nord5
+  fgd: '#d8dee9', // nord4
+  fgw: '#eceff4', // nord6
+
+  red: '#bf616a', // nord11
+}
+
 const THEME_STORAGE_KEY = 'start_page_theme'
 const DEFAULT_THEME_STORAGE = JSON.stringify({
-  colors: {
-    bg: '#282828',
-    bg1: '#3c3836',
-    bg2: '#504945',
-    fg: '#ebdbb2',
-    fgd: '#d5c4a1',
-    fgw: '#f2e5bc',
-    red: '#cc241d',
-  },
+  colors: nord,
 })
 
 const init = () => {
